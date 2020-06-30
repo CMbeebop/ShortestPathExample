d1752f2a8d4ebc90ea7efb74e5624f1152ddec5d
Carlos Monteserin <cmonteserin@hotmail.co.uk>
2020-06-30 19:31:48 +0200

    'FirstCommit'

---
+++ ShortestPathExample/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShortestPathExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            // Add my map of the states
+            PictureBox p = new PictureBox();
+            p.Image = new Bitmap("USWhiteMap");
+            p.Location = new Point(0,0);
+            this.Controls.Add(p);
+
+
+
+        }
+    }
+}
